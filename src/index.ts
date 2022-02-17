32a8ebb684f954dd16a5290baeeb1df37fb33a9e
aaronpangan <73965654+aaronpangan@users.noreply.github.com>
2022-02-18 03:51:18 +0800

    Apollo Server Switch
    Switch from apollo-server-express to apollo-server

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,19 @@
-import express from 'express';
 import 'dotenv/config';
 import { schema } from './graphql/registerSchema';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 async function startApolloServer() {
-  const app = express();
-
   const server = new ApolloServer({
     schema,
     introspection: true,
-   
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   });
 
-  await server.start();
-
-  app.get('/', (req, res) => res.send({ Meow: 'Meow' }));
-
-  server.applyMiddleware({ app, path: '/graphql' });
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(
-      `\n🚀      GraphQL is now running on http://localhost:${PORT}/graphql`,
+      `\n🚀      GraphQL is now running on http://localhost:${PORT}/`,
     );
   });
 }
