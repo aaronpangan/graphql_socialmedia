07c96bfb91632981caf2d6d991c795eccb55f725
aaronpangan <73965654+aaronpangan@users.noreply.github.com>
2022-02-18 05:38:21 +0800

    try apollo-server-express

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import 'dotenv/config';
+import express from 'express';
 import { schema } from './graphql/registerSchema';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 async function startApolloServer() {
+  const app = express();
+
   const server = new ApolloServer({
     schema,
     introspection: true,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   });
 
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
   const PORT = process.env.PORT || 5000;
   const env = process.env.NODE_ENV;
-  server.listen(PORT, () => {
+  app.listen(PORT, () => {
     if (env === 'development')
       console.log(
         `\n🚀      GraphQL is now running on http://localhost:${PORT}/`,
