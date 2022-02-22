2ca52989933a28aee2cd9cb01cf05a18cb92f3fe
aaronpangan <73965654+aaronpangan@users.noreply.github.com>
2022-02-22 13:33:37 +0800

    Add Post resolver

--- src/graphql/resolver/Post.resolver.ts
+++ src/graphql/resolver/Post.resolver.ts
@@ -1,4 +1,56 @@
+import { Post } from '@prisma/client';
+import { throwHttpGraphQLError } from 'apollo-server-core/dist/runHttpQuery';
+import { UserInputError } from 'apollo-server-express';
+import { ParamContext } from '../../interface/parameter.interface';
 
 module.exports = {
-  Query: {},
+  Query: {
+    getAllPost: async (_, __, { prisma }: ParamContext): Promise<Post[]> => {
+      const post = await prisma.post.findMany({
+        where: {
+          isPublic: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+      return post;
+    },
+    getOnePost: async (_, { id }, { prisma }: ParamContext): Promise<Post> => {
+      const post = await prisma.post.findFirst({
+        where: {
+          id,
+        },
+      });
+
+      if (!post) throw new UserInputError('Invalid argument value');
+
+      return post;
+    },
+    getUserPost: async (
+      _,
+      { userId },
+      { prisma }: ParamContext,
+    ): Promise<Post[]> => {
+      const user = await prisma.profile.findFirst({
+        where: {
+          userId,
+        },
+      });
+
+      if (!user) throw new UserInputError('User Not Found');
+
+      const post = await prisma.post.findMany({
+        where: {
+          isPublic: true,
+          profileId: userId,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+
+      return post;
+    },
+  },
 };
