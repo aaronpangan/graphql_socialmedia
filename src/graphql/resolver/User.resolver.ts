588c7e4fe5853e92a80d8b4832f817486c7b6fd5
aaronpangan <73965654+aaronpangan@users.noreply.github.com>
2022-02-23 04:20:46 +0800

    add get user

--- src/graphql/resolver/User.resolver.ts
+++ src/graphql/resolver/User.resolver.ts
@@ -1,3 +1,25 @@
+import { Profile, User } from '@prisma/client';
+import { GraphQLError } from 'graphql';
+import { ParamContext } from './../../interface/parameter.interface';
+import { UserInputError } from 'apollo-server-express';
 module.exports = {
   Query: {},
+
+  Profile: {
+    user: async (
+      parent: Profile,
+      _,
+      { prisma }: ParamContext,
+    ): Promise<User> => {
+      const user = await prisma.user.findFirst({
+        where: {
+          id: parent.userId,
+        },
+      });
+
+      if (!user) throw new UserInputError('User Not Found');
+
+      return user;
+    },
+  },
 };
